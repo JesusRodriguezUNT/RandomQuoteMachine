b4a20a019baa65bf43cc0370070e83d05ad525e7
JesusRodriguezUNT <32721119+JesusRodriguezUNT@users.noreply.github.com>
2019-10-06 13:21:40 -0500

    Adding docs folder

---
+++ docs/precache-manifest.9ef7eb29ceb61978d6062f5928e0836e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebdb411d879614b0fa2a3df9b3ceb5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1595d964330b82690a70",
+    "url": "/static/css/main.0742b1a5.chunk.css"
+  },
+  {
+    "revision": "7809ac5cbacecf676bc6",
+    "url": "/static/js/2.da45bdfb.chunk.js"
+  },
+  {
+    "revision": "1595d964330b82690a70",
+    "url": "/static/js/main.6692b60f.chunk.js"
+  },
+  {
+    "revision": "fca8267ef67765b90b9e",
+    "url": "/static/js/runtime-main.80696ad9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
