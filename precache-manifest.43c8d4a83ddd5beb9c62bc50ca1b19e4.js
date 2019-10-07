0e6c447733dfb08c5e1c4db00cf39b11cde78064
JesusRodriguezUNT <32721119+JesusRodriguezUNT@users.noreply.github.com>
2019-10-06 19:30:49 -0500

    Updates

---
+++ precache-manifest.43c8d4a83ddd5beb9c62bc50ca1b19e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8ed20101ef372a85152c8fcc008a39",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "11b44cf65022906a91ba",
+    "url": "/RandomQuoteMachine/static/css/main.0742b1a5.chunk.css"
+  },
+  {
+    "revision": "83e3b61dfc5285e30ac0",
+    "url": "/RandomQuoteMachine/static/js/2.3be061a5.chunk.js"
+  },
+  {
+    "revision": "11b44cf65022906a91ba",
+    "url": "/RandomQuoteMachine/static/js/main.bce03bec.chunk.js"
+  },
+  {
+    "revision": "849cb739ea48a9c3d75e",
+    "url": "/RandomQuoteMachine/static/js/runtime-main.caee1fa1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/RandomQuoteMachine/static/media/logo.25bf045c.svg"
+  }
+]);
